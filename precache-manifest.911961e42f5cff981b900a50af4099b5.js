d0577255afe280bad3327673cf0abe999f6f4134
Vogue <oprya1111@yandex.ru>
2020-09-25 21:09:38 +0300

    Updates

---
+++ precache-manifest.911961e42f5cff981b900a50af4099b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "848437cfae5ae2944a1812e488814b11",
+    "url": "/starwars-app/index.html"
+  },
+  {
+    "revision": "329ae5d0f8cfc05f2f6b",
+    "url": "/starwars-app/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "58288aaa0f24cf4f52cf",
+    "url": "/starwars-app/static/css/main.85e97f98.chunk.css"
+  },
+  {
+    "revision": "329ae5d0f8cfc05f2f6b",
+    "url": "/starwars-app/static/js/2.ef6b6f0f.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/starwars-app/static/js/2.ef6b6f0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58288aaa0f24cf4f52cf",
+    "url": "/starwars-app/static/js/main.7f62ab59.chunk.js"
+  },
+  {
+    "revision": "e3e047d430323263bd79",
+    "url": "/starwars-app/static/js/runtime-main.5f11bbda.js"
+  }
+]);
